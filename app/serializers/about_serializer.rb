ed58f86f864b3cfb4dba37073930fdfb6ea95a10
taiki-umetsu <taiki.umetsu.st@gmail.com>
2023-03-24 00:21:39 +0900

    create About model and API endpoint

---
+++ app/serializers/about_serializer.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class AboutSerializer < ActiveModel::Serializer
+  attributes :description
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resource :about, only: %i[show update]
+    end
+  end
 end
