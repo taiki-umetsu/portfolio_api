c1d0552587ba137bfdd690ef89bc761a264ff5e0
taiki-umetsu <taiki.umetsu.st@gmail.com>
2023-03-29 22:52:45 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,24 @@
 # frozen_string_literal: true
 
-User.find_or_create_by(email: "demo-user@example.com") do |user|
-  user.password = "demouserpassword"
+users = [
+  {
+    email: "demo-user@example.com",
+    password: "demouserpassword",
+    access_level: User::AccessLevel::READ_ONLY
+  },
+  {
+    email: "portfolio-ui@example.com",
+    password: "portfoliouipassword",
+    access_level: User::AccessLevel::READ_ONLY
+  },
+  { email: ENV.fetch("ADMIN_USER_EMAIL", nil),
+    password: ENV.fetch("ADMIN_USER_PASSWORD", nil),
+    access_level: User::AccessLevel::FULL }
+]
+
+users.each do |user|
+  User.find_or_create_by(email: user[:email]) do |u|
+    u.password = user[:password]
+    u.access_level = user[:access_level]
+  end
 end
