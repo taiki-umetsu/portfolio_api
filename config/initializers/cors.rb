2f512db004cde6ecdc360926c289f2d0e6f0ad55
taiki-umetsu <taiki.umetsu.st@gmail.com>
2023-03-30 23:35:07 +0900

    fix bit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins "portfolio.taikiumetsu.dev",
             "portfolio-admin.taikiumetsu.dev",
             "portfolio-admin-git-staging-taiki-umetsu.vercel.app",
-            "localhost:3000",
+            "localhost:8081",
             "localhost:8080"
     resource "*",
              headers: :any,
